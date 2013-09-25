ffd754cee590881be0dd6715dd7918160d86e7f5
cosc301 <cosc301@csvm.(none)>
2013-09-25 16:46:49 -0400

    done except for 1

--- fork03.c
+++ fork03.c
@@ -8,12 +8,23 @@
 int main(int argc, char **argv)
 {
         
-    char *cmd[] = { "/bin/ls", "-ltr", ".", NULL };
-
-    if (execv(cmd[0], cmd) < 0) {
-        fprintf(stderr, "execv failed: %s\n", strerror(errno));
-    }
-
-    return 0;
+	char *cmd[] = { "/bin/ls", "-ltr", ".", NULL };
+        pid_t pid = fork();
+	int status;
+	if (pid<0) { 
+		printf("%s\n","fork failed");
+		return -1;
+	}
+	else if (pid==0) { //child
+		printf("%s\n","child");
+		if (execv(cmd[0], cmd) < 0) {
+			fprintf(stderr, "execv failed: %s\n", strerror(errno));
+	    	}
+	}
+	else { //parent
+		waitpid(pid, &status, 0);
+		printf("%s\n","child process has finished");
+	}
+    	return 0;
 }
 
